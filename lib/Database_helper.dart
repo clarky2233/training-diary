e79d762a007fcb0d19c0f751ff876377ec2c14f6
Angus Clark <acla6624@uni.sydney.edu.au>
2020-01-29 16:01:17 +1100

    uplaod data 3

--- lib/Database_helper.dart
+++ lib/Database_helper.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:training_journal/DBUpgrades.dart';
@@ -86,7 +89,36 @@ class DBHelper {
 
   void setup() async {
     firestore = DatabaseService(uid: user.id);
-    database = createDatabase();
+    copyDatabase();
+    //database = createDatabase();
+  }
+
+  void copyDatabase() async {
+    var databasesPath = await getDatabasesPath();
+    var path = join(databasesPath, "journal_database.db");
+    var exists = await databaseExists(path);
+    if (!exists) {
+      // Should happen only the first time you launch your application
+      print("Creating new copy from asset");
+
+      // Make sure the parent directory exists
+      try {
+        await Directory(dirname(path)).create(recursive: true);
+      } catch (_) {}
+
+      // Copy from asset
+      ByteData data = await rootBundle.load(join("copy_journal_database.db"));
+      List<int> bytes =
+          data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+      // Write and flush the bytes written
+      await File(path).writeAsBytes(bytes, flush: true);
+    } else {
+      print("Opening existing database");
+    }
+    Database db = await openDatabase(path);
+    List<TrainingSession> x = await getJournal(db);
+    print(x.length);
   }
 
   Future<Database> createDatabase() async {
